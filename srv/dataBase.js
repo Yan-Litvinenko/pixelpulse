d6646d3d8c22be7300e700dd5cdebefa23ce6e98
Darth-VaderX <glavnyydirizhor@gmail.com>
2024-06-12 14:03:53 +0300

    fix: connect db v2

--- srv/dataBase.js
+++ srv/dataBase.js
@@ -3,24 +3,26 @@ const mysql = require('mysql2/promise');
 class DataBase {
     constructor(objectConfig) {
         this.config = objectConfig;
-        this.pool = mysql.createPool(this.config);
     }
 
-    async getConnection() {
-        try {
-            const connection = await this.pool.getConnection();
-            return connection;
-        } catch (error) {
-            console.error('Error getting database connection:', error);
-            throw error;
+    async connect() {
+        if (this.db) {
+            try {
+                await this.db.ping();
+            } catch (error) {
+                console.error('Lost connection, reconnecting:', error);
+                this.db = await mysql.createConnection(this.config);
+            }
+        } else {
+            this.db = await mysql.createConnection(this.config);
+            console.log('Database connection successful!');
         }
     }
 
     async getDataAdminTable(columnName) {
-        let connection;
         try {
-            connection = await this.getConnection();
-            const [rows] = await connection.query(`SELECT ${columnName} FROM admin LIMIT 1`);
+            await this.connect();
+            const [rows] = await this.db.query(`SELECT ${columnName} FROM admin LIMIT 1`);
             if (rows.length > 0) {
                 return rows[0][columnName];
             }
@@ -28,64 +30,52 @@ class DataBase {
         } catch (error) {
             console.error('Request execution error:', error);
             return null;
-        } finally {
-            if (connection) connection.release();
         }
     }
 
     async updateDataAdminTable(columnName, value) {
-        let connection;
         try {
-            connection = await this.getConnection();
-            await connection.execute(`UPDATE admin SET ${columnName} = ?`, [value]);
+            await this.connect();
+            await this.db.execute(`UPDATE admin SET ${columnName} = ?`, [value]);
             return true;
         } catch (error) {
             console.error('Request execution error:', error);
             return false;
-        } finally {
-            if (connection) connection.release();
         }
     }
 
     async updateUserTable(ip) {
-        let connection;
         try {
-            connection = await this.getConnection();
-            const [rows] = await connection.query(`SELECT COUNT(*) AS count FROM users WHERE ip_address = ?`, [ip]);
+            await this.connect();
+            const [rows] = await this.db.query(`SELECT COUNT(*) AS count FROM users WHERE ip_address = ?`, [ip]);
             const hasInDataBase = rows[0].count > 0;
 
             if (hasInDataBase) {
                 const query = `UPDATE users SET login_count = login_count + 1, last_login = CURRENT_TIMESTAMP WHERE ip_address = ?`;
-                await connection.execute(query, [ip]);
+                await this.db.execute(query, [ip]);
             } else {
                 const query = `INSERT INTO users (ip_address, login_count, last_login) VALUES (?, ?, CURRENT_TIMESTAMP)`;
-                await connection.execute(query, [ip, 1]);
+                await this.db.execute(query, [ip, 1]);
             }
         } catch (error) {
             console.error('Request execution error:', error);
-        } finally {
-            if (connection) connection.release();
         }
     }
 
     async getAchievements() {
-        let connection;
         try {
-            connection = await this.getConnection();
-            const [rows] = await connection.execute('SELECT * FROM achievements');
+            await this.connect();
+            const [rows] = await this.db.execute('SELECT * FROM achievements');
             return rows;
         } catch (error) {
             console.error('Request execution error:', error);
             return [];
-        } finally {
-            if (connection) connection.release();
         }
     }
 
     async achievedLevelAchieve(level) {
-        let connection;
         try {
-            connection = await this.getConnection();
+            await this.connect();
             const query =
                 'UPDATE achievements SET status = ?, date = ? WHERE description = ? AND status = "in progress"';
 
@@ -98,11 +88,9 @@ class DataBase {
             const day = this.getZero(dateObject.getDate());
             const date = `${year}.${month}.${day}`;
 
-            await connection.execute(query, [status, date, description]);
+            await this.db.execute(query, [status, date, description]);
         } catch (error) {
             console.error('Request execution error:', error);
-        } finally {
-            if (connection) connection.release();
         }
     }
 
@@ -121,9 +109,6 @@ const dataBase = new DataBase({
     user: '',
     password: '',
     database: '',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
 });
 
 module.exports = dataBase;
