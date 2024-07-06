a1e92d946f7fc55baacc3d77c00b9300843a22d6
Yan <glavnyydirizhor@gmail.com>
2024-07-06 13:53:53 +0300

    refactoring: about element

--- src/components/aboutElement/AboutElement.tsx
+++ src/components/aboutElement/AboutElement.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import Heading from '../heading/Heading';
-import Paragraph from '../paragraph/Paragraph';
 import styles from './AboutElement.module.scss';
 
 interface IAboutElement {
@@ -9,10 +7,12 @@ interface IAboutElement {
 }
 
 const AboutElement = (props: IAboutElement): React.JSX.Element => {
+    const { title, text } = props;
+
     return (
         <div className={styles.item}>
-            <Heading className={styles.item__title} level="3" textContent={props.title} />
-            <Paragraph className={styles.item__text} textContent={props.text} />
+            <h3 className={styles.item__title}>{title}</h3>
+            <p className={styles.item__text}>{text}</p>
         </div>
     );
 };
