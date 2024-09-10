17e3d68b76a96bf35cf3a351472b7138eab80558
Yan <glavnyydirizhor@gmail.com>
2024-09-10 21:07:27 +0300

    fix: sound click navigation menu

--- src/components/navigationElement/NavigationElement.tsx
+++ src/components/navigationElement/NavigationElement.tsx
@@ -2,51 +2,52 @@ import React from 'react';
 import styles from './NavigationElement.module.scss';
 import { Hexagon } from '../svgIcon/SvgIcon';
 import { mediaQuerySelector } from '../../store/selectors';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { scroll } from '../../classes/Scroll';
-import {
-    soundsClickTrigger,
-    soundsModalTrigger,
-} from '../../store/slices/soundsSlice';
+import { soundsClickTrigger, soundsModalTrigger } from '../../store/slices/soundsSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { useModal } from '../../hooks/useModal';
 import type { AppDispatch } from '../../store/store';
 import type { INavigationElement } from '../../interfaces/interface.component';
+import type { Page } from '../../interfaces/interface';
 
 const NavigationElement = (props: INavigationElement): React.JSX.Element => {
+    const location = useLocation();
     const dispatch = useDispatch<AppDispatch>();
     const closeNavigationMobile = useModal('navigationMobile').close;
 
     const { isMedium } = useSelector(mediaQuerySelector);
     const { pageName } = props;
 
-    const switchPage = (): void => {
+    const switchPage = (event: React.MouseEvent<HTMLLIElement, MouseEvent>): void => {
+        const from = location.pathname.replace('/', '');
+        const where = (event.target as HTMLElement).closest('li')?.querySelector('h4')?.textContent as Page;
+
         if (isMedium) {
-            dispatch(soundsModalTrigger());
+            if (where !== from) {
+                dispatch(soundsModalTrigger());
+            }
+
             closeNavigationMobile();
             scroll.on();
         } else {
-            dispatch(soundsClickTrigger());
+            if (where !== from) {
+                dispatch(soundsClickTrigger());
+            }
         }
     };
 
     return (
-        <li onClick={switchPage}>
+        <li onClick={(event) => switchPage(event)}>
             <NavLink
                 to={pageName}
-                className={({ isActive }) =>
-                    isActive
-                        ? `${styles.link_active} ${styles.link}`
-                        : styles.link
-                }
+                className={({ isActive }) => (isActive ? `${styles.link_active} ${styles.link}` : styles.link)}
             >
                 <h4 className={styles.link__title}>
                     {pageName}
                     <Hexagon />
                 </h4>
-                <p className={styles.link__text}>
-                    Suscipit est consequatur nemo voluptatem est labore saepe.
-                </p>
+                <p className={styles.link__text}>Suscipit est consequatur nemo voluptatem est labore saepe.</p>
             </NavLink>
         </li>
     );
